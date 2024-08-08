604de96a234cb8eb23fe86f7ccc08e78cfc9c065
LyJXD <xiaotskawaii@gmail.com>
2024-08-08 15:19:19 +0800

    0

--- Assets/PlayerPrimaryAttack.cs
+++ Assets/PlayerPrimaryAttack.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerPrimaryAttack : PlayerState
 {
+    private int comboCounter;
+
+    private float lastTimeAttacked;
+    private float comboWindow = 2;  // 连击窗口，攻击间隔不超过该时间则计算连击
+
     public PlayerPrimaryAttack(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
     }
@@ -12,12 +17,22 @@ public class PlayerPrimaryAttack : PlayerState
     {
         base.Enter();
 
+        if(comboCounter > 2 || Time.time >= lastTimeAttacked + comboWindow)
+        {
+            comboCounter = 0;
+        }
+
+        player.anim.SetInteger("ComboCounter", comboCounter);
+
         player.SetVelocity(0, 0);
     }
 
     public override void Exit()
     {
         base.Exit();
+
+        comboCounter++;
+        lastTimeAttacked = Time.time;
     }
 
     public override void Update()
